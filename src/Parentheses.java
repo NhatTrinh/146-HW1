c0a753e77ad0f4769e4e00d217bac183088ec626
Nhat Trinh <goodyshoes_nt@yahoo.com>
2017-02-07 21:18:22 -0800

    First commit

---
+++ src/Parentheses.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Nhat on 2/7/17.
+ */
+
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Parentheses {
+    static Stack<String> myStack = new Stack<String>();
+    static Scanner in = new Scanner(System.in);
+
+    public static String readInput() {
+        System.out.println("Enter parentheses: ");
+        String input = in.next();
+        return input;
+    }
+
+    public static void displayResult(String input){
+
+    }
+
+    public static void main (String args[]){
+
+    }
+
+
+}
