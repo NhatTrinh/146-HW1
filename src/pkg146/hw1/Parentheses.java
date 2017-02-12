9205b9c2a9ff65071ec777df536df0cf3035868d
Nhat M. Trinh <goodyshoes_nt@yahoo.com>
2017-02-12 09:18:27 -0800

    finished parentheses

--- src/pkg146/hw1/Parentheses.java
+++ src/pkg146/hw1/Parentheses.java
@@ -8,47 +8,68 @@ import java.util.Stack;
 import java.util.Scanner;
 
 public class Parentheses {
-    static Stack<String> stack = new Stack<String>();
+    
+    static Stack<Character> stack = new Stack<>();
     static Scanner in = new Scanner(System.in);
-
+    
+    public static boolean isBeginning(char arg){
+        if (arg == '<' || arg == '{' || arg == '[' || arg == '(')
+            return true;
+        return false;
+    }
+    
+    public static Character getOpposite(char arg){
+        char oppositeChar = ' ';
+        switch(arg){
+            case '>':
+                oppositeChar = '<';
+                break;
+            case ')':
+                oppositeChar = '(';
+                break;
+            case '}':
+                oppositeChar = '{';
+                break;
+            case ']':
+                oppositeChar = '[';
+                break;
+        }
+        return oppositeChar;
+    }
+    
     public static String readInput() {
         System.out.println("Enter parentheses: ");
         String input = in.next();
         return input;
     }
 
-    public static void evaluate(String input){
-        String stringElem;
-        Boolean isUnbalanced = false;
+    public static boolean evaluate(String input){
+        char top;
+        char current;
         
-        int i = 0;
-        int [] popIndexes = new int[input.length()];
-       
-        while (isUnbalanced == false && i < input.length()){
-            stringElem = input.substring(i,i+1);
-            System.out.println("substring: " + stringElem);
-            
-            if (stringElem.compareTo("[") == 0)
-                stack.push("]");
-            else if (stringElem.compareTo("{") == 0)
-                stack.push("}");
-            else if (stringElem.compareTo("<") == 0)
-                stack.push(">");
-            else if (stringElem.compareTo("(") == 0)
-                stack.push(")");
-            if (stack.search(stringElem) == -1 && i >= 1 && stack.isEmpty() == false){
-                isUnbalanced = true;
-                System.out.println("FALSE");
+        for (int i = 0; i < input.length(); i++){
+            current = input.charAt(i);
+            if (isBeginning(current))
+                stack.push(current);
+            else if (stack.isEmpty())
+                return false;
+            else{
+                top = stack.pop();
+                if (top != getOpposite(current))
+                    return false;
             }
-            i++;
+            
+            
         }
-        
-        
+        return true;
     }
-
+    
     public static void main (String args[]){
-        String userInput = readInput();
-        evaluate(userInput);
+        String input = readInput();
+        if (evaluate(input) == false)
+            System.out.println("False");
+        else
+            System.out.println("True");
     }
 
 
--- src/pkg146/hw1/Queue.java
+++ src/pkg146/hw1/Queue.java
@@ -3,6 +3,38 @@ package pkg146.hw1;
 
 import java.util.LinkedList;
 
-public class Queue {
+public class Queue<E> {
     
+    Node<E> last;
+    
+    public Queue(){
+        LinkedList <E> Queue = new LinkedList<>();
+        Node <E> first = new Node<>();
+        Node <E> last = new Node<>();
+        
+        
+    }
+    
+    public static class Node<E>{
+        E data;
+        Node next;
+        Node previous;
+    }
+    
+    public static void enqueue(){
+        
+    }
+    
+    public static Node dequeue(){
+        
+    }
+    
+    
+    public static void setNext(Node current, Node arg){current.next = arg;}
+    
+    public static Node getNext(Node arg){return arg.next;}
+    
+    public static Node getPrevious(Node arg){return arg.previous;}
+    
+    public static void setPrevious(Node current, Node arg){current.previous = arg;}
 }
