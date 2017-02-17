e7b331e59292e8df4d820691100537230a74f0c4
Nhat Trinh <goodyshoes_nt@yahoo.com>
2017-02-17 14:19:33 -0800

    Update Queue.java

--- src/pkg146/hw1/Queue.java
+++ src/pkg146/hw1/Queue.java
@@ -1,4 +1,8 @@
-
+/*
+Write a Queue implementation that uses a circular linked list, 
+which is the same as a linked list except that no links are null and the value of last.next is first whenever the list is not empty. 
+Keep only one Node instance variable (last).
+*/
 public class Queue<E>{
 
     Node last;
